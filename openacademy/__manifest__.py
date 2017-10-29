1dbab0077c139b04b1e47381f7770da59a6018f6
pecho <EugenPechurin@gmail.com>
2017-10-29 13:44:14 +0200

    Customise form view using XML

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "openacademy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        My first module for the odoo10""",
 
     'description': """
         Long description of module's purpose
